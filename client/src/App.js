137d39149a9711202f440c1544d4eaff68208592
Anastayzia Kerschen <anastayzia.kerschen@gmail.com>
2021-10-14 19:44:22 -0600

    more front bsed stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,28 @@
-import React from 'react'; 
-
+import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import recipes from './images/recipes.png';
 const App = () => {
     return (
-        <div>
-            <h1>App</h1>
-        </div>
+        <Container maxidth="lg">
+            <AppBar position="static" color="inherit">
+                <Typography varaint="h2" align="center">The People's Recipes</Typography>
+                <img src={recipes} alt="recipes" height="60" />
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={7}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     );
 }
 
