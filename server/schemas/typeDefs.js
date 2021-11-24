8d7ea6fc0d6013208d09a59677aea0d414be98b9
Anastayzia Kerschen <anastayzia.kerschen@gmail.com>
2021-11-23 21:19:58 -0700

    the kinks are starting to go away

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,10 +6,15 @@ const typeDefs = gql`
     username: String!
     email: String
   }
+
   
   type Auth {
     token: ID!
     user: User
+  }
+
+  type Query {
+    me: User
   }
 
   type Mutation {
