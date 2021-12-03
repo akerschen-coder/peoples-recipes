f00245672789079cc6479b75d26a07eac5e6bd7e
Anastayzia Kerschen <anastayzia.kerschen@gmail.com>
2021-12-02 21:56:01 -0700

    trying to get response from fetch

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,106 +1,98 @@
 import React, { useState } from "react";
 import {
-  Card,
-  CardColumns,
-  Col,
-  Jumbotron,
-  Container,
-  Form,
-  Button,
+    Card,
+    CardColumns,
+    Col,
+    Jumbotron,
+    Container,
+    Form,
+    Button,
 } from "react-bootstrap";
 import Auth from "../utils/auth";
 import { useMutation } from "@apollo/client";
 
 export default function Intro() {
-<<<<<<< HEAD
     // search
-    // handle search change 
-        // have to figure out exactly what were pulling to display
-    // form submit function 
-        //has to target form 
-=======
-  // search
-  // handle search change
-  // have to figure out exactly what were pulling to display
-  const [searchedRecipes, setSearchedRecipes] = useState([]);
->>>>>>> 01213e8004658d5c68b43e5fdc842998fb5b7e43
+    // handle search change
+    // have to figure out exactly what were pulling to display
+    const [searchedRecipes, setSearchedRecipes] = useState([]);
 
-  const [searchInput, setSearchInput] = useState("");
+    const [searchInput, setSearchInput] = useState("");
 
-  // formsubmit function
-  //has to target form
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
+    // formsubmit function
+    //has to target form
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
 
-    if (!searchInput) {
-      return false;
-    }
+        if (!searchInput) {
+            return false;
+        }
 
-    try {
-      const response = await fetch(
-        `https://api.edamam.com/api/recipes/v2?type=public&q="chicken"&app_id=b1be71ba561901dcc4bdbbd96d737834';`
-      );
+        try {
+            const response = await fetch(
+                `https://api.edamam.com/api/recipes/v2?type=public&q=${searchInput}&app_id=b1be71ba561901dcc4bdbbd96d737834';`, {
+                mode: "no-cors",
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': 'http://localhost:3000',
+                },
+            }
+            );
+            console.log(response);
+            if (!response.ok) {
+                throw new Error("something went wrong!");
+            }
 
-      if (!response.ok) {
-        throw new Error("something went wrong!");
-      }
 
-      const { items } = await response.json();
+            const { items } = await response.json();
 
-      const recipeData = items.map((recipe) => ({
-        foodId: recipe.id,
-      }));
+            const recipeData = items.map((recipe) => ({
+                foodId: recipe.id,
+            }));
 
-      setSearchedRecipes(recipeData);
-      setSearchInput("");
-    } catch (err) {
-      console.error(err);
-    }
-  };
+            setSearchedRecipes(recipeData);
+            setSearchInput("");
+        } catch (err) {
+            console.error(err);
+        }
+    };
 
-  //later, lets just get recipes up first
-  // function to save recipe
+    //later, lets just get recipes up first
+    // function to save recipe
 
-  return (
-    <>
-      <Jumbotron fluid className="text-light bg-dark">
-        <Container>
-<<<<<<< HEAD
-            <div>
-                home page 
-            </div>
-            
-=======
-          <h1>Search for Recipes!</h1>
-          <Form onSubmit={handleFormSubmit}>
-            <Form.Row>
-              <Col xs={12} md={8}>
-                <Form.Control
-                  name="searchInput"
-                  value={searchInput}
-                  onChange={(e) => setSearchInput(e.target.value)}
-                  type="text"
-                  size="lg"
-                  placeholder="Search for a recipe"
-                />
-              </Col>
-              <Col xs={12} md={4}>
-                <Button type="submit" variant="success" size="lg">
-                  Submit Search
-                </Button>
-              </Col>
-            </Form.Row>
-          </Form>
->>>>>>> 01213e8004658d5c68b43e5fdc842998fb5b7e43
-        </Container>
-      </Jumbotron>
-      <Container>
-        <h2>
-          {searchedRecipes.length
-            ? `Viewing ${searchedRecipes.length} results:`
-            : "Search for a recipe to begin"}
-        </h2>
-        {/* <CardColumns>
+    return (
+        <>
+            <Jumbotron fluid className="text-light bg-dark">
+                <Container>
+                    <h1>Search for Recipes!</h1>
+                    <Form onSubmit={handleFormSubmit}>
+                        <Form.Row>
+                            <Col xs={12} md={8}>
+                                <Form.Control
+                                    name="searchInput"
+                                    value={searchInput}
+                                    onChange={(e) => setSearchInput(e.target.value)}
+                                    type="text"
+                                    size="lg"
+                                    placeholder="Search for a recipe"
+                                />
+                            </Col>
+                            <Col xs={12} md={4}>
+                                <Button type="submit" variant="success" size="lg">
+                                    Submit Search
+                                </Button>
+                            </Col>
+                        </Form.Row>
+                    </Form>
+                </Container>
+            </Jumbotron>
+            <Container>
+                <h2>
+                    {searchedRecipes.length
+                        ? `Viewing ${searchedRecipes.length} results:`
+                        : "Search for a recipe to begin"}
+                </h2>
+                {/* <CardColumns>
           {searchedRecipes.map((recipe) => {
             return (
               <Card key={recipe.foodId} border="dark">
@@ -135,7 +127,7 @@ export default function Intro() {
             );
           })}
         </CardColumns> */}
-      </Container>
-    </>
-  );
+            </Container>
+        </>
+    );
 }
