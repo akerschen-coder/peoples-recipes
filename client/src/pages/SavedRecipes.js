c05f5bf6378a8cd0f4ad0d2582b9a07e010376ec
Anastayzia Kerschen <anastayzia.kerschen@gmail.com>
2021-12-05 19:27:46 -0700

    started the saved recipes page

--- client/src/pages/SavedRecipes.js
+++ client/src/pages/SavedRecipes.js
@@ -1,19 +1,78 @@
 import React from 'react';
-import { Container, Card, Button } from 'react-bootstrap';
-export default function SavedRecipes() {
+import { Container, Card, Button, CardColumns } from 'react-bootstrap';
+
+import { useMutation, useQuery } from '@apollo/client';
+
+import { GET_USER } from '../utils/queries';
+import { REMOVE_RECIPE } from '../utils/mutations';
+
+import Auth from '../utils/auth';
+import { removeRecipeId } from '../utils/localStorage';
+import { REMOVE_RECIPE } from '../utils/mutations';
+
+const SavedRecipe = () => {
+    const { loading, data } = useQuery(GET_USER);
+    const [removeRecipe, { error }] = useMutation(REMOVE_RECIPE);
+
+    const userData = data?.me || {};
+    console.log(Auth.getToken());
+
+    const handleDeleteRecipe = async (foodId) => {
+        const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+        if (!token) {
+            return false;
+        }
+
+        try {
+            const { data } = await removeRecipe({
+                variables: { foodId }
+            });
+
+
+            removeRecipeId(foodId);
+        } catch (err) {
+            console.error(error);
+        }
+    }
+
+
     return (
-        <Container>
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="holder.js/100px180" />
-                <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-            </Card>
-        </Container>
+        <>
+            <Jumbotron fluid className='text-light bg-dark'>
+                <Container>
+                    <h1> Viewing saved recipes!</h1>
+                </Container>
+            </Jumbotron>
+            <Container>
+                <h2>
+                    {userData.savedRecipes?.length
+                        ? `Viewing ${userData.savedRecipes.length} saved ${userData.savedRecipes.length === 1 ? 'recipe' : 'recipes'}:`
+                        : 'You have no saved recipes!'}
+                </h2>
+                <CardColumns>
+                    {userData.savedRecipes?.map((recipe) => {
+                        return (
+                            <Card key={recipe.foodId} style={{ width: '18rem' }}>
+                                {recipe.image ? 
+                                <Card.Img 
+                                src={recipe.image} 
+                                alt={`The image for ${recipe.label}`} 
+                                variant='top' /> : null}
+                                <Card.Body>
+                                    <Card.Title>{recipe.label}</Card.Title>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make up the bulk of
+                                        the card's content.
+                                    </Card.Text>
+                                    <Button variant="primary">Go somewhere</Button>
+                                </Card.Body>
+                            </Card>
+                        )
+                    })}
+
+                </CardColumns>
+            </Container>
+        </>
     )
 }
