90e13a7176ec281190668fce09d1d7e89d94b0b9
johnlundy94 <79663204+johnlundy94@users.noreply.github.com>
2021-12-06 19:47:38 -0700

    Fixed front end errors for savedReacipe component

--- client/src/pages/SavedRecipes.js
+++ client/src/pages/SavedRecipes.js
@@ -1,78 +1,96 @@
-import React from 'react';
-import { Container, Card, Button, CardColumns } from 'react-bootstrap';
+import React from "react";
+import {
+  Container,
+  Card,
+  Button,
+  CardColumns,
+  Jumbotron,
+} from "react-bootstrap";
 
-import { useMutation, useQuery } from '@apollo/client';
+import { useMutation, useQuery } from "@apollo/client";
 
-import { GET_USER } from '../utils/queries';
-import { REMOVE_RECIPE } from '../utils/mutations';
+import { GET_USER } from "../utils/queries";
+import { REMOVE_RECIPE } from "../utils/mutations";
 
-import Auth from '../utils/auth';
-import { removeRecipeId } from '../utils/localStorage';
-import { REMOVE_RECIPE } from '../utils/mutations';
+import Auth from "../utils/auth";
+import { removeRecipeId } from "../utils/localStorage";
 
 const SavedRecipe = () => {
-    const { loading, data } = useQuery(GET_USER);
-    const [removeRecipe, { error }] = useMutation(REMOVE_RECIPE);
+  const { loading, data } = useQuery(GET_USER);
+  const [removeRecipe, { error }] = useMutation(REMOVE_RECIPE);
 
-    const userData = data?.me || {};
-    console.log(Auth.getToken());
+  const userData = data?.me || {};
+  console.log(Auth.getToken());
 
-    const handleDeleteRecipe = async (foodId) => {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
+  const handleDeleteRecipe = async (foodId) => {
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-        if (!token) {
-            return false;
-        }
-
-        try {
-            const { data } = await removeRecipe({
-                variables: { foodId }
-            });
-
-
-            removeRecipeId(foodId);
-        } catch (err) {
-            console.error(error);
-        }
+    if (!token) {
+      return false;
     }
 
+    try {
+      await removeRecipe({
+        variables: { foodId: foodId },
+      });
 
-    return (
-        <>
-            <Jumbotron fluid className='text-light bg-dark'>
-                <Container>
-                    <h1> Viewing saved recipes!</h1>
-                </Container>
-            </Jumbotron>
-            <Container>
-                <h2>
-                    {userData.savedRecipes?.length
-                        ? `Viewing ${userData.savedRecipes.length} saved ${userData.savedRecipes.length === 1 ? 'recipe' : 'recipes'}:`
-                        : 'You have no saved recipes!'}
-                </h2>
-                <CardColumns>
-                    {userData.savedRecipes?.map((recipe) => {
-                        return (
-                            <Card key={recipe.foodId} style={{ width: '18rem' }}>
-                                {recipe.image ? 
-                                <Card.Img 
-                                src={recipe.image} 
-                                alt={`The image for ${recipe.label}`} 
-                                variant='top' /> : null}
-                                <Card.Body>
-                                    <Card.Title>{recipe.label}</Card.Title>
-                                    <Card.Text>
-                                        Some quick example text to build on the card title and make up the bulk of
-                                        the card's content.
-                                    </Card.Text>
-                                    <Button variant="primary">Go somewhere</Button>
-                                </Card.Body>
-                            </Card>
-                        )
-                    })}
+      removeRecipeId(foodId);
+    } catch (err) {
+      console.error(error);
+    }
+  };
 
-                </CardColumns>
-            </Container>
-        </>
-    )
-}
+  if (loading) {
+    return <h2>LOADING...</h2>;
+  }
+
+  return (
+    <>
+      <Jumbotron fluid className="text-light bg-dark">
+        <Container>
+          <h1> Viewing saved recipes!</h1>
+        </Container>
+      </Jumbotron>
+      <Container>
+        <h2>
+          {userData.savedRecipes?.length
+            ? `Viewing ${userData.savedRecipes.length} saved ${
+                userData.savedRecipes.length === 1 ? "recipe" : "recipes"
+              }:`
+            : "You have no saved recipes!"}
+        </h2>
+        <CardColumns>
+          {userData.savedRecipes?.map((recipe) => {
+            return (
+              <Card key={recipe.foodId} style={{ width: "18rem" }}>
+                {recipe.image ? (
+                  <Card.Img
+                    src={recipe.image}
+                    alt={`The image for ${recipe.label}`}
+                    variant="top"
+                  />
+                ) : null}
+                <Card.Body>
+                  <Card.Title>{recipe.label}</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Button
+                    variant="primary"
+                    onClick={() => handleDeleteRecipe(recipe.foodId)}
+                  >
+                    {" "}
+                    Delete this recipe
+                  </Button>
+                </Card.Body>
+              </Card>
+            );
+          })}
+        </CardColumns>
+      </Container>
+    </>
+  );
+};
+
+export default SavedRecipe;
